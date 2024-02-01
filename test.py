a5640e206af2d3107393ccf6f029eb367998230c
gortorozyan <gor656789gor@gmail.com>
2024-02-01 22:47:25 +0400

    new files

---
+++ test.py
@@ -0,0 +1,37 @@
+import telebot
+import requests
+
+API_KEY = open('API_KEY').read()
+SEARCH_ENGINE_ID = open('SEARCH_ENGINE_ID').read()
+bot = telebot.TeleBot('6605728457:AAH4zd-aG0D3Px4qY_NufSbftmFRbVoL_KY')
+
+@bot.message_handler()
+def info(message):
+    search_query = message.text.lower()
+    url = 'https://www.googleapis.com/customsearch/v1'
+    params = {
+        'q':search_query,
+        'key':API_KEY,
+        'cx':SEARCH_ENGINE_ID,
+        'searchType':'image'
+    }
+    response = requests.get(url, params=params)
+    results = response.json()['items']
+    for item in results:
+        bot.send_photo(message.chat.id, item["link"])
+        
+
+
+# @bot.message_handler(commands=['start'])
+# def main(message):
+#     bot.send_message(message.chat.id, "gooo")
+
+
+
+bot.polling(none_stop=True) 
+
+
+
+
+
+
